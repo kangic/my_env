af7eec2ba4a06e2b7f595081421bcdce558b6cb2
Kang In Cheol <kangic21@gmail.com>
2022-02-04 21:44:04 +0900

    * update

--- install.sh
+++ install.sh
@@ -1,38 +1,57 @@
 #/bin/bash
 
 # global variables
-root_path=$HOME
+target_path=$HOME
+myenv_path=$PWD
 
 # check the arguments
 if [ $# -ne 0 ]; then
-    root_path=$1
+    target_path=$1
 fi
-echo "root_path : $root_path"
-
-# install git
-sudo apt install git -y
-
-# create workspace & clone my git repos
-mkdir ~/worksapce/github
-cd ~/workspace/github
-git clone https://github.com/kangic/dot_files
-git clone https://github.com/kangic/dot_vimrc
-
-# zsh
-sudo apt install zsh -y
+echo "target path : $target_path"
+echo "current path : $myenv_path"
 
 # entry point
 echo $1
 echo $2
 
+exit 0
+
+##########################################################
+## Install packages
+
+# install git, curl
+echo "install git, curl..."
+sudo apt install git curl -y
+
 # i3-gaps
+echo "install i3-gaps..."
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt update
 sudo apt install i3-gaps -y
 
+# i3lock-fancy
+echo "install i3lock-fancy..."
+sudo apt install i3lock-fancy -y
+
+# compton for i3 window's transparency
+echo "install compton..."
+sudo apt install compton -y
+
+# feh for setting the background image
+echo "install feh..."
+sudo apt install feh -y
+
 # tmux
+echo "install tmux..."
 sudo apt install tmux -y
-ln -s ${HOME}/github.com/kangic/dot_files/
+ln -s ${HOME}/
+
+# rofi
+echo "install rofi..."
+sudo apt install rofi -y
+
+# nvim
 
 # fonts
 git clone https://github.com/ryanoasis/nerd-fonts --depth 1 ~/Downloads
@@ -46,5 +65,9 @@ git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
 source ~/.bashrc
 
+
+##########################################################
+## Settings
+
 # update 
-#source ~/.zshrc
+source ~/.bashrc
